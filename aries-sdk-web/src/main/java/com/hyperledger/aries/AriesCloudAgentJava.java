f447e54274130759c3eccf0c22ac01f9ce93ebe5
Sukalpo Mitra <sukalpomitra@gmail.com>
2020-02-10 22:06:50 +0800

    Aries cloud agent starter classes
    Signed-off-by: Sukalpo Mitra <sukalpomitra@gmail.com>

--- aries-sdk-web/src/main/java/com/hyperledger/aries/AriesCloudAgentJava.java
+++ aries-sdk-web/src/main/java/com/hyperledger/aries/AriesCloudAgentJava.java
@@ -2,10 +2,12 @@ package com.hyperledger.aries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@PropertySource(value = "META-INF/MANIFEST.MF", ignoreResourceNotFound =  true)
 public class AriesCloudAgentJava {
     public static void main(final String args[]) {
         SpringApplication.run(AriesCloudAgentJava.class, args);
