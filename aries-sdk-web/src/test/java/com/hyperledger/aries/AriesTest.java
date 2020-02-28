0a1c1c656b8ffa7912d62b296aa5bbd5873a2bfa
Sukalpo Mitra <sukalpomitra@gmail.com>
2020-02-29 01:35:30 +0800

    Added test framework (BDT - Cucumber)
    Signed-off-by: Sukalpo Mitra <sukalpomitra@gmail.com>

---
+++ aries-sdk-web/src/test/java/com/hyperledger/aries/AriesTest.java
@@ -0,0 +1,10 @@
+package com.hyperledger.aries;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources")
+public class AriesTest {
+}
---
+++ aries-sdk-web/src/test/resources/aries.feature
@@ -0,0 +1,4 @@
+Feature: AriesJavaSDk
+  Scenario: Aries Java SDK Web Module
+    Given The web module has compiled properly
+    Then Aries Web SDK Spring Boot Application should be up
